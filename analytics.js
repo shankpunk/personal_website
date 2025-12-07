c5e9521e37e3d9005c31eaadcc0dcf5dfbae1042
shankar <6280498+shankpunk@users.noreply.github.com>
2025-12-07 20:17:39 +0530

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,5 @@
-<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "f45f4d7c09df457fb971f70107a6d447"}'></script><!-- End Cloudflare Web Analytics -->
+const s = document.createElement("script");
+s.src = "https://static.cloudflareinsights.com/beacon.min.js";
+s.defer = true;
+s.setAttribute("data-cf-beacon", '{"token": "f45f4d7c09df457fb971f70107a6d447"}');
+document.head.appendChild(s);
